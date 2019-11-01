594807d2e80bee0fb50717f6f19780f01b07bae1
NamHyun Gu <mnhan0403@gmail.com>
2019-11-01 16:42:38 +0900

    Add generate indices to all collection for search

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,52 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const COLLECTION_CELEBS = "celebs";
+const COLLECTION_PROGRAMS = "programs";
+const COLLECTION_RESTAURANTS = "restaurants";
+
+admin.initializeApp();
+
+exports.onCelebCreated = functions.firestore
+  .document(`${COLLECTION_CELEBS}/{celebId}`)
+  .onCreate((snapshot, context) => {
+    const data = snapshot.data();
+    if (data) {
+      // data.objectId = context.params.celebId;
+      data.indices = generateIndices(data.name);
+      return snapshot.ref.update(data);
+    }
+    return null;
+  });
+
+exports.onProgramCreated = functions.firestore
+  .document(`${COLLECTION_PROGRAMS}/{programId}`)
+  .onCreate((snapshot, context) => {
+    const data = snapshot.data();
+    if (data) {
+      // data.objectId = context.params.programId;
+      data.indices = generateIndices(data.name);
+      return snapshot.ref.update(data);
+    }
+    return null;
+  });
+
+exports.onRestaurantCreated = functions.firestore
+  .document(`${COLLECTION_RESTAURANTS}/{restaurantId}`)
+  .onCreate((snapshot, context) => {
+    const data = snapshot.data();
+    if (data) {
+      // data.objectId = context.params.restaurantId;
+      data.indices = generateIndices(data.name);
+      return snapshot.ref.update(data);
+    }
+    return null;
+  });
+
+const generateIndices = (name: String) => {
+  const indices = [];
+  for (let end = 1; end <= name.length; end++) {
+    indices.push(name.substring(0, end));
+  }
+  return indices;
+};

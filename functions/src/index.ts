0dbe988ecb76a29442ad1b49e1ee9e2d73ec4fe0
NamHyun Gu <mnhan0403@gmail.com>
2019-11-01 18:17:06 +0900

    Add update relations

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,8 @@ import * as admin from "firebase-admin";
 const COLLECTION_CELEBS = "celebs";
 const COLLECTION_PROGRAMS = "programs";
 const COLLECTION_RESTAURANTS = "restaurants";
+const COLLECTION_CELEBS_RELATIONS = "celebs_relations";
+const COLLECTION_PROGRAMS_RELATIONS = "programs_relations";
 
 admin.initializeApp();
 
@@ -12,7 +14,24 @@ exports.onCelebCreated = functions.firestore
   .onCreate((snapshot, context) => {
     const data = snapshot.data();
     if (data) {
-      // data.objectId = context.params.celebId;
+      const dataId = context.params.celebId;
+
+      // Update relations
+      admin
+        .firestore()
+        .collection(COLLECTION_CELEBS_RELATIONS)
+        .doc(dataId)
+        .create({})
+        .then(_ =>
+          console.debug(`Update relations ${COLLECTION_CELEBS}/${dataId}`)
+        )
+        .catch(err =>
+          console.error(
+            `Can't update relations ${COLLECTION_CELEBS}/${dataId}`,
+            err
+          )
+        );
+
       data.indices = generateIndices(data.name);
       return snapshot.ref.update(data);
     }
@@ -24,7 +43,24 @@ exports.onProgramCreated = functions.firestore
   .onCreate((snapshot, context) => {
     const data = snapshot.data();
     if (data) {
-      // data.objectId = context.params.programId;
+      const dataId = context.params.programId;
+
+      // Update relations
+      admin
+        .firestore()
+        .collection(COLLECTION_PROGRAMS_RELATIONS)
+        .doc(dataId)
+        .create({})
+        .then(_ =>
+          console.debug(`Update relations ${COLLECTION_PROGRAMS}/${dataId}`)
+        )
+        .catch(err =>
+          console.error(
+            `Can't update relations ${COLLECTION_PROGRAMS}/${dataId}`,
+            err
+          )
+        );
+
       data.indices = generateIndices(data.name);
       return snapshot.ref.update(data);
     }

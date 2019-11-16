8c059c84fb7d055b54f371fa853d410d9945c709
NamHyun Gu <mnhan0403@gmail.com>
2019-11-16 18:16:02 +0900

    Remove update relations collection

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,8 +4,6 @@ import * as admin from "firebase-admin";
 const COLLECTION_CELEBS = "celebs";
 const COLLECTION_PROGRAMS = "programs";
 const COLLECTION_RESTAURANTS = "restaurants";
-const COLLECTION_CELEBS_RELATIONS = "celebs_relations";
-const COLLECTION_PROGRAMS_RELATIONS = "programs_relations";
 
 admin.initializeApp();
 
@@ -14,24 +12,6 @@ exports.onCelebCreated = functions.firestore
   .onCreate((snapshot, context) => {
     const data = snapshot.data();
     if (data) {
-      const dataId = context.params.celebId;
-
-      // Update relations
-      admin
-        .firestore()
-        .collection(COLLECTION_CELEBS_RELATIONS)
-        .doc(dataId)
-        .create({})
-        .then(_ =>
-          console.debug(`Update relations ${COLLECTION_CELEBS}/${dataId}`)
-        )
-        .catch(err =>
-          console.error(
-            `Can't update relations ${COLLECTION_CELEBS}/${dataId}`,
-            err
-          )
-        );
-
       data.indices = generateIndices(data.name);
       return snapshot.ref.update(data);
     }
@@ -43,24 +23,6 @@ exports.onProgramCreated = functions.firestore
   .onCreate((snapshot, context) => {
     const data = snapshot.data();
     if (data) {
-      const dataId = context.params.programId;
-
-      // Update relations
-      admin
-        .firestore()
-        .collection(COLLECTION_PROGRAMS_RELATIONS)
-        .doc(dataId)
-        .create({})
-        .then(_ =>
-          console.debug(`Update relations ${COLLECTION_PROGRAMS}/${dataId}`)
-        )
-        .catch(err =>
-          console.error(
-            `Can't update relations ${COLLECTION_PROGRAMS}/${dataId}`,
-            err
-          )
-        );
-
       data.indices = generateIndices(data.name);
       return snapshot.ref.update(data);
     }
